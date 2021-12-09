43fd1ba060fe3ac7467d4e92e425fbabc4dbcf3b
Luke <luke.colussi8@gmail.com>
2021-12-09 18:46:15 -0500

    Match Making + CLI
    Created a working match making function as well as a CLI for user interaction. Messed with some functions and what they print out.

--- Project5/Graph.h
+++ Project5/Graph.h
@@ -9,25 +9,23 @@
 #include <iostream>
 #include <set>
 #include "DisjointSet.h"
+
 using namespace std;
 
 class Graph
 {
 private:
 	unordered_map<string, vector<pair<string, Game>>> adjList;
-	void visit(string name, int& elo, double& wl);
 	
 public:
 	Graph();
 	void insertGame(Game game);
 	void matchmake(string name);
-	// Functions below are mainly for debug, not necessarily actual proper graph algorithms or even well implemented
 	void printNames(int n = INT_MAX);
 	void printWins(string name);
-	vector<string> getAdjacent(string vertex);
-	void primAlgorithm(string name, Graph graph);
+	void getAdjacent(string vertex);
+	void primAlgorithm(string name);
 	int getElo(string vertex);
-	int sumOfMST();
 };
 
 Graph::Graph() {}
@@ -38,17 +36,30 @@ void Graph::insertGame(Game game)
 	adjList[game.whiteID].push_back(newGame1);
 	pair<string, Game> newGame2(game.whiteID, game);
 	adjList[game.blackID].push_back(newGame2);
-
-
 }
 
 void Graph::matchmake(string name)
 {
+	vector<string> goodMatches;
 	unordered_set<string> marked;
 	queue<string> q;
 	q.push(name);
 	marked.insert(name);
 	int counter = 0;
+	int requestedElo = 0;
+	if (adjList[name].at(0).second.blackID == name)
+	{
+		requestedElo = stoi(adjList[name].at(0).second.blackElo);
+	}
+	else if (adjList[name].at(0).second.whiteID == name)
+	{
+		requestedElo = stoi(adjList[name].at(0).second.whiteElo);
+	}
+
+	int minElo = INT_MAX;
+	int maxMatches = 0;
+	int closestElo = 0;
+	int requestedMatches = 0;
 	while (!q.empty())
 	{
 		string current = q.front();
@@ -60,7 +71,12 @@ void Graph::matchmake(string name)
 		int maxTime = 0;
 		for (pair<string, Game> p : adjList[current])
 		{
+			if (current == name)
+			{
+				requestedMatches++;
+			}
 			bool isWhite = current == p.second.whiteID;
+
 			bool won = isWhite == stoi(p.second.whiteRatingDiff) > 0;
 			if (won)
 				wins++;
@@ -69,7 +85,7 @@ void Graph::matchmake(string name)
 			unsigned int time = p.second.getDateTime();
 			if (time > maxTime)
 			{
-				time = maxTime;
+				maxTime = time;
 				elo = isWhite ? stoi(p.second.whiteElo) : stoi(p.second.blackElo);
 			}
 			if (marked.find(p.first) == marked.end())
@@ -77,116 +93,151 @@ void Graph::matchmake(string name)
 				q.push(p.first);
 				marked.insert(p.first);
 			}
+
+			if (abs(elo - requestedElo) < minElo && (current != name))
+			{
+				if (abs((wins + losses) - requestedMatches) > maxMatches)
+				{
+					goodMatches.clear();
+					minElo = abs(elo - requestedElo);
+					maxMatches = abs((wins + losses) - requestedMatches);
+					goodMatches.push_back(current);
+					closestElo = elo;
+				}
+			}
 		}
-		double wl = wins / (wins + losses);
-		cout << "name: " << current << " wl: " << wl << " elo: " << elo << endl;
+
+
+		double wl = (double)wins / ((double)wins + losses);
 
 		counter++;
 		if (counter > 20)
 			break;
 	}
+
+	if (goodMatches.empty())
+	{
+		cout << "Could not find a good match for: " << name << "." << endl;
+	}
+	else
+	{
+		cout << "We were given " << name << " with a current elo of " << requestedElo << " and " << requestedMatches << " matches played." << endl;
+		cout << "The best match is " << goodMatches.at(0) << " with an elo of " << closestElo << " and " << maxMatches << " matches played." << endl;
+	}
 }
 
-vector<string> Graph::getAdjacent(string vertex)
+void Graph::getAdjacent(string vertex)
 {
-	vector<string> v;
+	cout << "Adjacent nodes:" << endl;
 	for (int i = 0; i < adjList[vertex].size(); i++)
 	{
-		v.push_back(adjList[vertex].at(i).first);
+		cout << i << ": " << adjList[vertex].at(i).first << endl;
 	}
-	return v;
+
 }
 
 int Graph::getElo(string vertex)
 {
-	pair<string, Game> p = adjList[vertex].at(0);
-	return stoi(p.second.blackRatingDiff);
+	vector<pair<string, Game>> opponents = adjList[vertex];
+	unsigned int maxTime = 0;
+	int elo = -1;
+	for (pair<string, Game> p : opponents)
+	{
+		unsigned int time = p.second.getDateTime();
+		if (time > maxTime)
+		{
+			maxTime = time;
+			elo = vertex == p.second.whiteID ? stoi(p.second.whiteElo) : stoi(p.second.blackElo);
+		}
+	}
+	return elo;
 }
-//void Graph::visit(string name, int& elo, double& wl)
+
+
+//void Graph::primAlgorithm(string name)
 //{
-//	int maxTime = 0;
-//	for (pair<string, Game> p : adjList[name])
+//	set<string> T;
+//	set<string> adjacents;
+//	T.insert(name);
+//	vector<pair<string, Game>> adjacent = adjList[name];
+//
+//	string minName = "";
+//	int minElo = INT_MAX;
+//	for (int i = 0; i < adjacent.size(); i++)
 //	{
-//		bool isWhite = name == p.second.whiteID;
-//		bool won = isWhite == stoi(p.second.whiteRatingDiff) > 0;
-//		wl = won ? wl + 1 : wl - 1;
-//		unsigned int time = p.second.getDateTime();
-//		if (time > maxTime)
-//		{
-//			time == maxTime;
-//			elo = isWhite ? stoi(op.second.whiteElo) : stoi(op.second.blackElo);
-//		}
+//		adjacents.insert(adjacent[i].first);
+//
 //	}
+//
+//	while (!adjacents.empty())
+//	{
+//		for (int i = 1; i < adjacent.size() - 1; i++)
+//		{
+//			if (getElo(adjacent.at(i).first) < minElo && T.find(adjacent.at(i).first) == T.end())
+//			{
+//				minElo = getElo(adjacent.at(i).first);
+//				minName = adjacent.at(i).first;
+//			}
+//		}
+//
+//		T.insert(minName);
+//		adjacents.erase(minName);
+//		minElo = INT_MAX;
+//	}
+//
+//	for (auto itr = T.begin(); itr != T.end(); ++itr)
+//	{
+//		cout << *itr << " ";
+//	}
+//
 //}
 
-
-void Graph::primAlgorithm(string name, Graph graph)
+void Graph::primAlgorithm(string name)
 {
 	set<string> T;
-	set<string> adjacents;
-	string id = name;
-	T.insert(id);
-	vector<string> adjacent = graph.getAdjacent(id);
+	unordered_map<string, int> keys;
+	unordered_map<string, string> MST;
+	for (auto i = adjList.begin(); i != adjList.end(); i++)
+		keys[i->first] = INT_MAX;
+	keys[name] = 0;
+	int safety = 0;
 
-	string minName = "";
-	int minElo = INT_MAX;
-	for (int i = 1; i < adjacent.size() - 1; i++)
+	while (T.size() != adjList.size())
 	{
-		adjacents.insert(adjacent.at(i));
-
-	}
-
-	while (!adjacents.empty())
-	{
-		for (int i = 1; i < adjacent.size() - 1; i++)
+		int min = INT_MAX;
+		string key;
+		for (auto i = keys.begin(); i != keys.end(); i++)
 		{
-			if (graph.getElo(adjacent.at(i)) < minElo && T.find(adjacent.at(i)) == T.end())
+			if (T.find(i->first) == T.end() && i->second < min)
 			{
-				minElo = graph.getElo(adjacent.at(i));
-				minName = adjacent.at(i);
+				min = i->second;
+				key = i->first;
 			}
 		}
-
-		T.insert(minName);
-		adjacents.erase(minName);
-		minElo = INT_MAX;
-	}
-
-	for (auto itr = T.begin(); itr != T.end(); ++itr)
-	{
-		cout << *itr << " ";
-	}
-
-}
-
-int Graph::sumOfMST()
-{
-	DisjointSet ds;
-	ds.makeSet(vertices);
-	int res = 0;
-
-	for (const auto& entry : graph) {
-		int c = entry.first; // weight 
-		pair<int, int> p = entry.second; // two vertices 
-		int u = p.first;
-		int v = p.second;
-		int x = ds.findRoot(u);
-		int y = ds.findRoot(v);
-		if (x != y) {
-			res += c;
-			ds.unionSet(x, y);
+		T.insert(key);
+		for (pair<string, Game> p : adjList[key])
+		{
+			int weight = p.second.whiteID == p.first ? stoi(p.second.whiteRatingDiff) : stoi(p.second.blackRatingDiff);
+			keys[p.first] = std::min(keys[p.first], weight);
+			MST[p.first] = key;
 		}
+		safety++;
+		if (safety % 10 == 0)
+			cout << key;
+		if (safety > INT_MAX)
+			throw std::invalid_argument("Infinite while loop");
 	}
 
-	return res;
+	cout << "DONE" << endl;
 }
+
 
 void Graph::printNames(int n)
 {
 	int count = 0;
 	for (auto iter = adjList.begin(); iter != adjList.end(); iter++)
 	{
-		cout << iter->first << endl;
+		cout << count << ": " << iter->first << endl;
 
 		count++;
 		if (count >= n)
@@ -196,9 +247,13 @@ void Graph::printNames(int n)
 
 void Graph::printWins(string name)
 {
+	int i = 1;
 	for (pair<string, Game> p : adjList[name])
 	{
+		cout << "Game " << i << ":" << endl;
 		string ratingDiff = p.second.winner == p.second.whiteID ? p.second.whiteRatingDiff : p.second.blackRatingDiff;
-		cout << "Opponent: " << p.first << " Rating Diff: " << ratingDiff << " Time: " << p.second.getDateTime() << endl;
+		cout << "Game mode:" << p.second.event << "| Opponent: " << p.first << " | Rating Diff: " << ratingDiff << " | Date: " << p.second.date << endl;
+		i++;
+		cout << endl;
 	}
 }
--- Project5/main.cpp
+++ Project5/main.cpp
@@ -8,10 +8,11 @@ using namespace std;
 int main()
 {
 	//Number of games to be read from CSV
-	cout << "Input N = ";
+	cout << "Please put the number of data points to use: ";
 	int n;
 	cin >> n;
 
+	cout << "Reading " << n << " rows of data..." << endl;
 	//Creates graph and loads in data
 	Reader reader("chess_games.csv");
 	Graph graph;
@@ -21,12 +22,79 @@ int main()
 		graph.insertGame(g);
 	}
 
-	cout << "ENTER NAME" << endl;
+	//debug stuff, prints first 500 names in graph and asks for a user whose opponents can be listed
+	/*if (n < 500)
+		graph.printNames();
+	else
+	{
+		graph.printNames(500);
+		cout << "(only first 500 printed)" << endl;
+	}
+	*/
+
+	cout << endl;
+
+	cout << "Features:" << endl;
+	cout << "1) Display all names in the graph." << endl;
+	cout << "2) Display all adjacent nodes of a specific player." << endl;
+	cout << "3) Print the stats of every game the person has played. " << endl;
+	cout << "4) Find the best match for a specific player." << endl;
+	cout << "5) Compare Prim and Kruskal Algorithms." << endl;
+	cout << endl;
+	cout << "Enter the corresponding number for each feature: ";
+	
+	int option = -1;
+	cin >> option;
+
+	cout << endl;
+
 	string name;
-	cin >> name;
-	graph.printWins(name);
-	graph.matchmake(name);
-	graph.matchmakeDfs(name);
+	switch (option)
+	{
+		case -1:
+			break;
+		case 1:
+
+			cout << "Enter how many names to print. Enter 0 to show all names in the graph." << endl;
+			int number;
+			cin >> number;
+			cout << endl;
+			if (number == 0)
+			{
+				graph.printNames(n);
+			}
+			else
+			{
+				graph.printNames(number);
+			}
+			break;
+
+		case 2:
+
+			cout << "Please enter a name to use: ";
+			cin >> name;
+
+			cout << endl;
+			graph.getAdjacent(name);
+			break;
+		case 3:
+			cout << "Please enter a name to use: ";
+			cin >> name;
+
+			cout << endl;
+			graph.printWins(name);
+			break;
+		case 4:
+			cout << "Please enter a name to use: ";
+			cin >> name;
+
+			cout << endl;
+			graph.matchmake(name);
+			break;
+		case 5:
+
+			break;
+	}
 
 	return 0;
 }
